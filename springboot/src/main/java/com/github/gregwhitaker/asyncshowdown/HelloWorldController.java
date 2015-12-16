15fd9fdd115fd975fdd6f0319984ab46b8eca4d7
Whitaker, Greg <Greg.Whitaker@nike.com>
2015-12-16 11:49:24 -0800

    Implemented SpringBoot application

---
+++ springboot/src/main/java/com/github/gregwhitaker/asyncshowdown/HelloWorldController.java
@@ -0,0 +1,49 @@
+package com.github.gregwhitaker.asyncshowdown;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+import rx.Observable;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+@RestController
+public class HelloWorldController {
+    private static Random RANDOM = new Random(System.currentTimeMillis());
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public DeferredResult<ResponseEntity<String>> hello(@RequestParam(name = "minSleep", defaultValue = "0") long minSleep,
+                                                        @RequestParam(name = "maxSleep", defaultValue = "0") long maxSleep) {
+        final DeferredResult<ResponseEntity<String>> deferredResult = new DeferredResult<ResponseEntity<String>>();
+
+        final FutureTask<String> helloTask = new FutureTask(new HelloTask(minSleep, maxSleep));
+        Observable.from(helloTask)
+                .last()
+                .subscribe(message -> deferredResult.setResult(ResponseEntity.ok(message)),
+                           error -> deferredResult.setResult(ResponseEntity.status(500).body(error.getMessage())));
+                
+        return deferredResult;
+    }
+
+    /**
+     * Task that sleeps for a random amount of time, within a configurable interval, and then returns the string "Hello World!".
+     */
+    class HelloTask implements Callable<String> {
+        private final long duration;
+
+        public HelloTask(final long minSleep, final long maxSleep) {
+            this.duration = minSleep + (long)(RANDOM.nextDouble() * (maxSleep - minSleep));
+        }
+
+        @Override
+        public String call() throws Exception {
+            Thread.sleep(duration);
+            return "Hello World!";
+        }
+    }
+}
--- springboot/src/main/java/com/github/gregwhitaker/asyncshowdown/springboot/Application.java
+++
@@ -1,4 +0,0 @@
-package com.github.gregwhitaker.asyncshowdown.springboot;
-
-public class Application {
-}
