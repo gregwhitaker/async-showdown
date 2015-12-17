fa4c08898faa475f66fa98edfcdd7b9cda1e7f71
Whitaker, Greg <Greg.Whitaker@nike.com>
2015-12-17 11:38:55 -0800

    Added path handler to undertow example

--- undertow/src/main/java/com/github/gregwhitaker/asyncshowdown/Application.java
+++ undertow/src/main/java/com/github/gregwhitaker/asyncshowdown/Application.java
@@ -1,13 +1,15 @@
 package com.github.gregwhitaker.asyncshowdown;
 
 import io.undertow.Undertow;
+import io.undertow.server.handlers.PathHandler;
 
 public class Application {
 
     public static void main(String... args) {
         Undertow server = Undertow.builder()
                 .addHttpListener(8080, "localhost")
-                .setHandler(new HelloHandler())
+                .setHandler(new PathHandler()
+                        .addExactPath("/hello", new HelloHandler()))
                 .build();
 
         server.start();
